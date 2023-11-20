1d0a7763d3f8c094ba8d0a0010ea52fb4d663d79
Mia Wittenmeier <mia.wittenmeier@students.hs-mainz.de>
2023-11-20 16:18:27 +0100

    add script for hearts spawn points

---
+++ MentalHell/Assets/Scripts/HeartSpawnManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartSpawnManager : MonoBehaviour
+{
+    // this script randomly selects three of the hearts 
+
+    private List<GameObject> spawnPoints;
+
+    private void Start()
+    {
+        spawnPoints = new List<GameObject>(GameObject.FindGameObjectsWithTag("Heart"));
+    }
+
+    public void SpawnHearts()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            spawnPoints.RemoveAt(Random.Range(0, spawnPoints.Count)); 
+        }
+
+        foreach (GameObject heart in spawnPoints)
+        {
+            heart.SetActive(false);
+        }
+    }
+}
