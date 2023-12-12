5a8854f33a82feecef0d40e2e97d24913235f3c4
Mia Wittenmeier <mia.wittenmeier@students.hs-mainz.de>
2023-12-12 12:33:54 +0100

    fix door float bug

--- MentalHell/Assets/Scripts/DoorManager.cs
+++ MentalHell/Assets/Scripts/DoorManager.cs
@@ -6,11 +6,16 @@ using UnityEngine;
 public class DoorManager : MonoBehaviour
 {
     [SerializeField] float positionX;
-    [SerializeField] float positionZ;
 
     public void EnterRoom(GameObject character)
     {
-        //Debug.Log("entered room");
-        character.transform.position = character.transform.position + new Vector3(positionX, 0.0f, positionZ);
+        if (character.transform.position.z <= 0)
+        {
+            character.transform.position = character.transform.position + new Vector3(positionX, 0.0f, 59.37f);
+        }
+        else
+        {
+            character.transform.position = character.transform.position + new Vector3(positionX, 0.0f, -59.37f);
+        }
     }
 }
