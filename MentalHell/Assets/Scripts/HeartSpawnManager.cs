0401bedaa1e3d9167a941801ca7756c269994a29
Mia Wittenmeier <mia.wittenmeier@students.hs-mainz.de>
2023-12-19 12:09:17 +0100

    fix heart spawn bug

--- MentalHell/Assets/Scripts/HeartSpawnManager.cs
+++ MentalHell/Assets/Scripts/HeartSpawnManager.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class HeartSpawnManager : MonoBehaviour
 {
-    // this script randomly selects three of the hearts 
+    // this script randomly selects three of the hearts placed in the game
 
     private List<GameObject> spawnPoints;
 
     private void Start()
     {
         spawnPoints = new List<GameObject>(GameObject.FindGameObjectsWithTag("Heart"));
+        SpawnHearts();
     }
 
     public void SpawnHearts()
     {
         for (int i = 0; i < 3; i++)
         {
-            spawnPoints.RemoveAt(Random.Range(0, spawnPoints.Count)); 
+            spawnPoints.RemoveAt(Random.Range(0, spawnPoints.Count));
         }
 
         foreach (GameObject heart in spawnPoints)
