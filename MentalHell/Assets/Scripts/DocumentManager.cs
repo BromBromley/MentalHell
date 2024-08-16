d4c2fe9a2e240cfac7029b13e4468e68a381f081
Brom <m.wittenmeier@gmx.net>
2024-08-16 14:24:23 +0200

    add new documents to inventory

--- MentalHell/Assets/Scripts/DocumentManager.cs
+++ MentalHell/Assets/Scripts/DocumentManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DocumentManager : MonoBehaviour
 {
@@ -13,12 +14,23 @@ public class DocumentManager : MonoBehaviour
 
     [SerializeField] private GameObject documentList;
     public GameObject documentScreen;
+    [SerializeField] private GameObject interactIcon;
     public bool showingInventory = false;
+
+    private PlayerInteraction _playerInteraction;
+
+
+
+    void Awake()
+    {
+        _playerInteraction = FindObjectOfType<PlayerInteraction>();
+        documents = new List<GameObject>(GameObject.FindGameObjectsWithTag("Document"));
+        allDocuments = new List<GameObject>(GameObject.FindGameObjectsWithTag("Document"));
+    }
+
 
     void Start()
     {
-        documents = new List<GameObject>(GameObject.FindGameObjectsWithTag("Document"));
-        allDocuments = new List<GameObject>(GameObject.FindGameObjectsWithTag("Document"));
         CloseAllDocuments();
 
         for (int i = 0; i < documentList.transform.childCount; i++)
@@ -28,6 +40,8 @@ public class DocumentManager : MonoBehaviour
         documentScreen.SetActive(false);
     }
 
+
+
     // picks a random document to show when the player interacts with storage
     public void ShowRandomDocument()
     {
@@ -36,17 +50,37 @@ public class DocumentManager : MonoBehaviour
         documentList.transform.Find(documents[index].name).gameObject.SetActive(true);
     }
 
-    // opens or closes the screen, called by GameManager
+
+
+    // checks if inventory is open, called by GameManager
+    public bool ActivatingInventory()
+    {
+        if (documentScreen.activeSelf)
+        {
+            CloseInventory();
+            return false;
+        }
+
+        OpenInventory();
+        return true;
+    }
+
+
     public void OpenInventory()
     {
         documentScreen.SetActive(true);
         showingInventory = true;
     }
+
     public void CloseInventory()
     {
+        CloseAllDocuments();
         documentScreen.SetActive(false);
         showingInventory = false;
     }
+
+
+
 
     public void CloseAllDocuments()
     {
@@ -60,9 +94,27 @@ public class DocumentManager : MonoBehaviour
         }
     }
 
+
     public void CloseDocument()
     {
         documents[index].SetActive(false);
         documents.RemoveAt(index);
+    }
+
+
+    // called when the player opens a document through the inventory
+    public void OpenDocument(GameObject documentButton)
+    {
+        foreach (GameObject document in allDocuments)
+        {
+            if (document.name == documentButton.name)
+            {
+                _playerInteraction.showingDocument = true;
+                document.SetActive(true);
+                documentScreen.SetActive(false);
+                interactIcon.SetActive(false);
+                showingInventory = false;
+            }
+        }
     }
 }
