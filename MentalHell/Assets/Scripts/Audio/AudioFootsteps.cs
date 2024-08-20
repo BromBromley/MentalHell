4c5c14c7c5a2c6f4b9a533ea0d24ed4a53048676
Janoti! <janoti@gmx.de>
2024-08-21 00:16:10 +0200

    Adjust Fottstep Script for new Animations

--- MentalHell/Assets/Scripts/Audio/AudioFootsteps.cs
+++ MentalHell/Assets/Scripts/Audio/AudioFootsteps.cs
@@ -5,7 +5,7 @@ public class AudioFootsteps : MonoBehaviour
 
     private SpriteRenderer animationSprites;
     private Sound[] Soundarray;
-    private string[] footstepNames = {"mentalhell_running__0000", "mentalhell_running__0008", "mentalhell_walk_0", "mentalhell_walk_4"};
+    private string[] footstepNames = {"run_0009", "run_0001", "walk_0009", "walk_0001"};
     private Sprite spriteCheck;
 
     void Start()
@@ -21,21 +21,26 @@ public class AudioFootsteps : MonoBehaviour
 
         for (int i = 0; i < footstepNames.Length; i++)
         {
+
             // check if sprite names are same as ones from array (sprites when turned into string have a Unit.Object addon thats why StartsWith)
             if (animationSprites.sprite.ToString().StartsWith(footstepNames[i])){
+
+                
                 
                 // set sprite equal to check to check that the sprite sound wont get played twice
                 // update is faster than the animation
                 spriteCheck = animationSprites.sprite;
 
+                Debug.Log("Walk.");
+
                 // check if sprite name starts with walk then play walk sound
-                if (animationSprites.sprite.ToString().StartsWith("mentalhell_walk")){
+                if (animationSprites.sprite.ToString().StartsWith("walk_")){
                     Soundarray = FindObjectOfType<AudioManager>().sfxStepsWalk;
                     FindObjectOfType<AudioManager>().PlayRandomOnce(Soundarray);
                 }
 
                 // check if sprite name starts with run then play run sound
-                if (animationSprites.sprite.ToString().StartsWith("mentalhell_run")){
+                if (animationSprites.sprite.ToString().StartsWith("run_")){
                     Soundarray = FindObjectOfType<AudioManager>().sfxStepsRun;
                     FindObjectOfType<AudioManager>().PlayRandomOnce(Soundarray);
                 }
